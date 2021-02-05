36303fe662c6a88f188d2a52a95d05f7d76a2b33
hoo <vicjed@icloud.com>
2021-02-05 09:59:10 +0900

    8-1.JPQL Criteria 활용(실무 사용 X)

--- src/main/java/com/test/ex1_hello_jpa/JpaMain.java
+++ src/main/java/com/test/ex1_hello_jpa/JpaMain.java
@@ -7,6 +7,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -20,7 +23,16 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            List<Member> resultList = em.createQuery("select m from Member m WHERE m.username LIKE '%kim%'", Member.class).getResultList();
+            //사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            //루트 클래스(조회를 시작한 클래스)
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+
+            List<Member> resultList = em.createQuery(cq).getResultList();
+
 
 
 
